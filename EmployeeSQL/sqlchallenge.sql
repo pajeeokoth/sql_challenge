af84d804c27b1a2d9f7539b5f17b143ac1159d4f
ajeeraba <ajeeraba3869837@users.noreply.github.com>
2023-04-23 19:49:46 -0400

    initial commit

---
+++ EmployeeSQL/sqlchallenge.sql
@@ -0,0 +1,150 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS  dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Create the tables and assign variable types, primary keys, and fpreign keys
+CREATE TABLE departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no, dept_no
+     )
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR   NOT NULL,
+	emp_no INTEGER   NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        emp_no, dept_no
+     )
+);
+
+CREATE TABLE employees (
+    emp_no INTEGER   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER   NOT NULL,
+    salary INTEGER   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+	CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES dept_emp (emp_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES salaries (emp_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES salaries (emp_no);
+
+ALTER TABLE titles ADD CONSTRAINT fk_titles_title_id FOREIGN KEY(title_id)
+REFERENCES employees (emp_title_id);
+
+
+-- No 1
+--List the employee number, last name, first name, sex, and salary of each employee.
+select 
+a.emp_no, a.last_name, a.first_name, a.sex, b.salary 
+from employees as a
+JOIN salaries as b on a.emp_no = b.emp_no;
+
+--No 2
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+select 
+last_name, first_name, hire_date 
+from employees
+where extract(year from hire_date) = 1986;
+
+--No 3
+--List the manager of each department along with their 
+-- department number, department name, employee number, last name, and first name.
+select
+a.dept_no, b.dept_name, c.emp_no, c.last_name, c.first_name 
+from dept_manager as a
+join departments as b on b.dept_no = a.dept_no
+JOIN employees as c on c.emp_no = a.emp_no;
+
+--No 4
+-- List the department number for each employee along with that 
+-- employee’s employee number, last name, first name, and department name.
+select 
+a.dept_no, c.emp_no, c.last_name, c.first_name, b.dept_name
+from employees as c
+join dept_emp as a on a.emp_no = c.emp_no
+join departments as b on b.dept_no = a.dept_no;
+
+--No 5
+-- List first name, last name, and sex of each employee whose 
+-- first name is Hercules and whose last name begins with the letter B.
+select 
+first_name, last_name, sex
+from employees
+where first_name like 'Hercules' and last_name like 'B%';
+
+--No 6
+-- List each employee in the Sales department, including their 
+--employee number, last name, and first name.
+select 
+a.emp_no, a.last_name, a.first_name, c.dept_name
+from employees as a
+join dept_emp as b on b.emp_no = a.emp_no
+join departments as c on c.dept_no = b.dept_no
+where c.dept_name LIKE 'Sales';
+
+-- No 7
+-- List each employee in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+select 
+a.emp_no, a.last_name, a.first_name, c.dept_name
+from employees as a
+inner join dept_emp as b on b.emp_no = a.emp_no
+inner join departments as c on c.dept_no = b.dept_no
+where c.dept_name LIKE 'Sales' or c.dept_name LIKE 'Development';
+
+-- No 8
+-- List the frequency counts, in descending order, of all the employee 
+-- last names (that is, how many employees share each last name).
+select 
+last_name, 
+count(*) AS frequency
+from employees
+group by last_name
+ORDER  BY frequency DESC;
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sql_challenge
+in this challenge, we will 
+    - create an employees database
+
+    - add tables to it
+
+    - define the primary and foreign keys and composite keys where needed
+
+    - import data from csv files into the tables
+
+    - create queries
+    
+    - use querries to summarize the data.
